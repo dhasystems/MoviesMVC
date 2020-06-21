0ba6a9f2295dba8f062e9b63c12c9b1d27c8005c
dencio2020 <dennis.alegre2014@gmail.com>
2020-06-21 08:17:31 +0800

    Add project files.

---
+++ Movies/Controllers/HomeController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Movies.Models;
+
+namespace Movies.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult GetList()
+        {
+            using (DBModel db = new DBModel())
+            {
+                var movlist = db.lists.ToList<list>();
+                return Json(new { data = movlist }, JsonRequestBehavior.AllowGet);
+            }
+        }
+
+       
+
+    }
+}
---
+++ Movies/Controllers/MoviesController.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Movies.Models;
+
+namespace Movies.Controllers
+{
+    public class MoviesController : Controller
+    {
+        // GET: Movies
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult GetData()
+        {
+            using (DBModel db = new DBModel())
+            {
+                List<list> movList = db.lists.ToList<list>();
+                return Json(new { data = movList }, JsonRequestBehavior.AllowGet);
+            }
+        }
+
+        [HttpGet]
+        public ActionResult AddOrEdit(int id = 0)
+        {
+            if (id == 0)
+                return View(new list());
+            else
+            {
+                using (DBModel db = new DBModel())
+                {
+                    return View(db.lists.Where(x => x.id == id).FirstOrDefault<list>());
+                }
+            }
+        }
+
+        [HttpPost]
+        public ActionResult AddOrEdit(list mov)
+        {
+            using (DBModel db = new DBModel())
+            {
+                if (mov.id == 0)
+                {
+                    db.lists.Add(mov);
+                    db.SaveChanges();
+                    return Json(new { success = true, message = "Saved Successfully" }, JsonRequestBehavior.AllowGet);
+                    
+                }
+                else
+                {
+                    db.Entry(mov).State = EntityState.Modified;
+                    db.SaveChanges();
+                    return Json(new { success = true, message = "Updated Successfully" }, JsonRequestBehavior.AllowGet);
+                }
+            }
+
+
+        }
+
+        [HttpPost]
+        public ActionResult Delete(int id)
+        {
+            using (DBModel db = new DBModel())
+            {
+                list mov = db.lists.Where(x => x.id == id).FirstOrDefault<list>();
+                db.lists.Remove(mov);
+                db.SaveChanges();
+                return Json(new { success = true, message = "Deleted Successfully" }, JsonRequestBehavior.AllowGet);
+            }
+        }
+
+        public ActionResult LogOut()
+        {
+            //Session["username"] = null;
+            Session.Clear();
+            return RedirectToAction("Index", "Home");
+
+        }
+
+    }
+}
